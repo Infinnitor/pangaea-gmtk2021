b4016999c1c87fe06683c14f75ded81bcdfd7596
Infinnitor <43831721+Infinnitor@users.noreply.github.com>
2021-06-11 22:25:50 +0100

    We added da moving man

--- game.py
+++ game.py
@@ -6,26 +6,91 @@ pygame.init()
 
 
 class game_info():
-    def __init__(self, win_w, win_h, bg_colour):
+    def __init__(self, win_w, win_h, chunks, start_chunk):
 
         self.win_w = win_w
         self.win_h = win_h
 
         self.win = pygame.display.set_mode((win_w, win_h))
-        pygame.display.set_caption("GMTK game jam")
+        pygame.display.set_caption("Pangaea's wacky camera adventure")
 
-        self.bg = bg_colour
+        self.chunks = chunks
+
+        self.current_chunk = start_chunk
+
+    def update_keys(self):
+
+        self.keys = pygame.key.get_pressed()
+
+#   |||
+#   |||
+#   |||
 
 
-game = game_info(win_w=1200, win_h=720, bg_colour=(0, 0, 0))
+class map_chunk():
+    def __init__(self, index, name, countries, borders_dict, bg):
+
+        self.index = index
+        self.name = name
+
+        self.countries = countries
+        self.borders = borders_dict
+
+        self.bg = bg
+
+    def update_draw(self, game):
+        game.win.fill(self.bg)
+
+
+class player():
+    def __init__(self, start_x, start_y, speed, start_width, start_height, sprites_dict):
+
+        self.x = start_x
+        self.y = start_y
+        self.speed = speed
+
+        self.width = start_width
+        self.height = start_height
+
+        self.sprites = sprites_dict
+
+    def update_move(self, game):
+        if game.keys[pygame.K_LEFT]:
+            self.x -= self.speed
+
+        if game.keys[pygame.K_RIGHT]:
+            self.x += self.speed
+
+        if game.keys[pygame.K_UP]:
+            self.y -= self.speed
+
+        if game.keys[pygame.K_DOWN]:
+            self.y += self.speed
+
+    def update_draw(self, game):
+        pygame.draw.rect(game.win, (155, 40, 40), (self.x, self.y, self.width, self.height))
+
+
+game = game_info(win_w=1200, win_h=720, chunks=(None, None, None, None, None, None))
+
+
+pangea = player(start_x=600, start_y=400, speed=5, start_width=100, start_height=100, start_chunk=0, sprites_dict=None)
 
 run = True
-while run:
-    pygame.time.delay(10)
 
-    pygame.draw.rect(game.win, (155, 40, 40), (0, 0, game.win_w // 5, game.win_h // 5))
+clock = pygame.time.Clock()
+
+while run:
+
+    clock.tick(60)
+
     pygame.display.update()
-    game.win.fill(game.bg)
+    game.win.fill((0, 0, 0))
+    game.update_keys()
+
+    game.chunks[game.current_chunk].update_draw(game)
+    pangea.update_move(game)
+    pangea.update_draw(game)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
