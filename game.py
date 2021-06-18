d0e6532aa0c82043b4f073d5e551884a6d74c252
infinnitor <infinnitor@gmail.com>
2021-06-18 15:13:20 +0100

    Remove useless second import of os

--- game.py
+++ game.py
@@ -1,11 +1,9 @@
-from os import environ
-environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-
 import math
 import random
 
 import sys
 import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 import pygame
 pygame.init()
